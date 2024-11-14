54a03db186343e9325cde8fa2902c07d19ce77f2
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-11-14 16:40:57 -0500

    add comments to members of TableModel

--- EuchreTournamentLibrary/TableModel.cs
+++ EuchreTournamentLibrary/TableModel.cs
@@ -2,10 +2,30 @@ namespace EuchreTournamentLibrary;
 
 public class TableModel
 {
+    /// <summary>
+    /// Refers to a specific location for the table.
+    /// Identifies where the players' match is located.
+    /// </summary>
     public int Number { get; set; }
+    
+    /// <summary>
+    /// Represents the round to which this table belongs.
+    /// Each table can only belong to one round.
+    /// </summary>
     public RoundModel Round { get; set; }
+    
+    /// <summary>
+    /// Represents the two teams that will play at this table.
+    /// Teams are transient and only exist within their current round.
+    /// </summary>
     public List<TeamModel> Teams { get; set; }
-
+    
+    /// <summary>
+    /// Instantiates a new table given an identifying number, a round and two teams.
+    /// </summary>
+    /// <param name="number">Identifies where the players' match is located.</param>
+    /// <param name="round">Represents the round to which this table belongs.</param>
+    /// <param name="teams">Represents the two teams that will play at this table.</param>
     public TableModel(int number, RoundModel round, List<TeamModel> teams)
     {
         Number = number;
