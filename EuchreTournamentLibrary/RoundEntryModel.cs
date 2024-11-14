f14b87c1a6cad07e56bd998e41ac918747445876
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-11-14 15:51:08 -0500

    add RoundEntryModel

--- EuchreTournamentLibrary/RoundEntryModel.cs
+++ EuchreTournamentLibrary/RoundEntryModel.cs
@@ -2,5 +2,16 @@ namespace EuchreTournamentLibrary;
 
 public class RoundEntryModel
 {
-    
+    public PlayerModel ReportingPlayer { get; set; }
+    public TeamModel ReportingTeam { get; set; }
+    public int RoundNumber { get; set; }
+    public int Score { get; set; }
+
+    public RoundEntryModel(PlayerModel reportingPlayer, TeamModel reportingTeam, int roundNumber, int score)
+    {
+        ReportingPlayer = reportingPlayer;
+        ReportingTeam = reportingTeam;
+        RoundNumber = roundNumber;
+        Score = score;
+    }
 }
