6532d98276118d0caaddb23fb6a83b69236dd46e
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-11-14 15:24:59 -0500

    add TotalScore property

--- EuchreTournamentLibrary/PlayerModel.cs
+++ EuchreTournamentLibrary/PlayerModel.cs
@@ -4,13 +4,15 @@ public class PlayerModel
 {
     
     public string PlayerName { get; set; }
-    public Dictionary<Round, int > RoundScore { get; set; }
+    public Dictionary<RoundModel, int > RoundScore { get; set; }
     public bool IsHost { get; set; }
+    public int TotalScore { get; set; }
     
     public PlayerModel(string playerName, bool isHost)
     {
         this.PlayerName = playerName;
         this.IsHost = isHost;
-        this.RoundScore = new Dictionary<Round, int>();
+        this.RoundScore = new Dictionary<RoundModel, int>();
+        this.TotalScore = 0;
     }
 }
