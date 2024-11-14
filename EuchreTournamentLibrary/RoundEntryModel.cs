a36ede88033caa1a6bb01c4fc2b8a01e2d33e2fb
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-11-14 16:36:11 -0500

    add comments to members of RoundEntryModel

--- EuchreTournamentLibrary/RoundEntryModel.cs
+++ EuchreTournamentLibrary/RoundEntryModel.cs
@@ -2,11 +2,33 @@ namespace EuchreTournamentLibrary;
 
 public class RoundEntryModel
 {
+    /// <summary>
+    /// Represents the player who submitted the entry.
+    /// </summary>
     public PlayerModel ReportingPlayer { get; set; }
+    
+    /// <summary>
+    /// Represents the team on behalf of which the entry was submitted.
+    /// </summary>
     public TeamModel ReportingTeam { get; set; }
+    
+    /// <summary>
+    /// Represents the round to which the entry belongs.
+    /// </summary>
     public int RoundNumber { get; set; }
+    
+    /// <summary>
+    /// Represents the points earned by the ReportingTeam in the RoundNumber.
+    /// </summary>
     public int Score { get; set; }
 
+    /// <summary>
+    /// Instantiates a new round entry from a player, team, round number and score.
+    /// </summary>
+    /// <param name="reportingPlayer">Represents the player who submitted the entry.</param>
+    /// <param name="reportingTeam">Represents the team on behalf of which the entry was submitted.</param>
+    /// <param name="roundNumber">Represents the round to which the entry belongs.</param>
+    /// <param name="score">Represents the points earned by the ReportingTeam in the RoundNumber.</param>
     public RoundEntryModel(PlayerModel reportingPlayer, TeamModel reportingTeam, int roundNumber, int score)
     {
         ReportingPlayer = reportingPlayer;
