48fe5fad4b8118318d073265aa5981b5f94e60e4
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-11-14 16:11:35 -0500

    add comments to PlayerModel

--- EuchreTournamentLibrary/PlayerModel.cs
+++ EuchreTournamentLibrary/PlayerModel.cs
@@ -2,17 +2,37 @@ namespace EuchreTournamentLibrary;
 
 public class PlayerModel
 {
-    
+    /// <summary>
+    /// Represents the name that will identify the player.
+    /// </summary>
     public string PlayerName { get; set; }
-    public Dictionary<RoundModel, int > RoundScore { get; set; }
+    
+    /// <summary>
+    /// Represents each entry associated with a player. See RoundEntryModel for all associated properties.
+    /// </summary>
+    public List<RoundEntryModel> RoundEntries { get; set; }
+    
+    /// <summary>
+    /// Represents the host status of player. True represents a host.
+    /// There may only be one host per Room. 
+    /// </summary>
     public bool IsHost { get; set; }
+    
+    /// <summary>
+    /// Represents the sum of all completed round score integers.
+    /// </summary>
     public int TotalScore { get; set; }
     
+    /// <summary>
+    /// Instantiates a new player with a provided name and host status.
+    /// </summary>
+    /// <param name="playerName"> Represents the name that will identify the player. </param>
+    /// <param name="isHost"> Represents the sum of all completed round score integers. </param>
     public PlayerModel(string playerName, bool isHost)
     {
         this.PlayerName = playerName;
         this.IsHost = isHost;
-        this.RoundScore = new Dictionary<RoundModel, int>();
+        this.RoundEntries = new List<RoundEntryModel>();
         this.TotalScore = 0;
     }
 }
