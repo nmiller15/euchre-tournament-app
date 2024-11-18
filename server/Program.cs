88ab0fdc6d2584db1a0c335597c35c7912d4b7aa
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-11-18 16:12:59 -0500

    update dev settings

--- server/Program.cs
+++ server/Program.cs
@@ -4,11 +4,15 @@ string serverAddress = "127.0.0.1";
 var port = 8080;
 var server = new WebSocketServer($"ws://{serverAddress}:{port}/");
 
+var wsConnections = new List<IWebSocketConnection>();
+var users = new List<>();
+
 server.Start(ws =>
 {
     ws.OnOpen = () =>
     {
         Console.WriteLine($"Connected");
+        wsConnections.Add(ws);
     };
     ws.OnClose = () =>
     {
@@ -17,6 +21,10 @@ server.Start(ws =>
     ws.OnMessage = message =>
     {
         Console.WriteLine($"Received: {message}");
+        foreach (var connection in wsConnections)
+        {
+            connection.Send(message);
+        }
     };
 });
 
