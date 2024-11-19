2eb82a618cc6bee684c25cf468e6f1241a0505cf
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-11-19 08:36:29 -0500

    add comments to properties

--- EuchreTournamentLibrary/MessageModel.cs
+++ EuchreTournamentLibrary/MessageModel.cs
@@ -2,25 +2,42 @@ namespace EuchreTournamentLibrary;
 
 public class MessageModel
 {
+    /// <summary>
+    /// Represents the type of message that will be sent to the client. This is used by the client to make decisions
+    /// on what to do with the incoming message.
+    /// </summary>
     public string Type { get; set; }
     
+    /// <summary>
+    /// Represents a text string describing the content of the payload, or to perform string interactions with the
+    /// client.
+    /// </summary>
     public string Message { get; set; }
     
-    public string? StringPayload { get; set; }
-    
+    /// <summary>
+    /// A payload option for a message, contains a RoomModel.
+    /// </summary>
     public RoomModel? RoomPayload { get; set; }
     
+    /// <summary>
+    /// A payload option for a message, contains a UserModel.
+    /// </summary>
     public UserModel? UserPayload { get; set; }
     
+    /// <summary>
+    /// A payload option for a message, contains a RoundModel.
+    /// </summary>
     public RoundModel? RoundPayload { get; set; }
     
+    /// <summary>
+    /// A payload option for a message, contains a TeamModel.
+    /// </summary>
     public TeamModel? TeamPayload { get; set; }
 
-    public MessageModel(string type, string message, string payload)
+    public MessageModel(string type, string message)
     {
         Type = type;
         Message = message;
-        StringPayload = payload;
     }
     
     public MessageModel(string type, string message, RoomModel payload)
