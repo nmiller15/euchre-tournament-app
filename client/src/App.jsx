c945b9b8bb6e83ca5998e48cb8ee3f772db14343
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-11-25 09:36:28 -0500

    add useWebSocket and get connected

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Login from "./components/Login";
 import Lobby from "./Lobby";
 import Results from "./Results";
 import Tournament from "./Tournament";
 import { roomModel } from "../mock/roomModel";
 import { results } from "../mock/results";
+import useWebSocket from "react-use-websocket";
 
 function App() {
+  const WS_URL = "ws://127.0.0.1:8080";
+  const { sendJsonMessage, lastJsonMessage } = useWebSocket(WS_URL, {
+    share: true,
+    queryParams: {
+      username: "user1",
+      createRoom: "true",
+    },
+  });
+
+  useEffect(() => {
+    sendJsonMessage({
+      message: "Hello",
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log(lastJsonMessage);
+  }, [lastJsonMessage]);
+
   return (
     <div className="flex min-h-screen justify-center">
       <div className="flex w-[360px] justify-center bg-slate-100">
